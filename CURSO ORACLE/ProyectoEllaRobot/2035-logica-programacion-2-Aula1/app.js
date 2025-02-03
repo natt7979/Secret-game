5d15d5fca33f00d9fb3be72d23b6c3e3859c7ac1
Natalia Fonseca <fonseca79natalia@gmail.com>
2025-02-02 19:44:41 -0600

    Delete CURSO ORACLE directory

--- CURSO ORACLE/ProyectoEllaRobot/2035-logica-programacion-2-Aula1/app.js
+++
@@ -1,5 +0,0 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML ="Juego del numero secreto";
-
-let parrafo = document.querySelector("p");
-parrafo.innerHTML = "Indica un numero del 1-10";

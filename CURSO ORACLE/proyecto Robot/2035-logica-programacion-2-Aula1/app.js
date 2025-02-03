5a0e85fc6cbe5bc8b27d2fd57bed00ae2fdecc44
Natalia Fonseca <fonseca79natalia@gmail.com>
2025-02-02 19:36:49 -0600

    Version final juego del numero secreto
    Primer proyecto

---
+++ CURSO ORACLE/proyecto Robot/2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,11 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+
+console.log(numeroUsuario);
+if (numeroUsuario == numeroSecreto) {
+    alert( "Acertaste el numero es :NumeroUsuario" );
+}
+else {
+    ("Lo siento no acertaste el numero");
+}
+
